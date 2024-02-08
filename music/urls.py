4bc40f2df67890a3569ea3d3c0a42a01e1343465
denisagapov <denisagapov6666@gmail.com>
2024-02-08 20:29:28 +0100

    1 change

--- music/urls.py
+++ music/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import url
+from django.urls import path, include
 from . import views
 
 app_name = 'music'
 
 urlpatterns = [
 	#/music/
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    path(r'^$', views.IndexView.as_view(), name='index'),
 
     #/music/{album_id}/
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
     #/music/album/add
-    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+    path(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 
 ]
